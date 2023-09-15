0e8396e339fda26143fd35df291feb4814a07573
Jason O'Gray <1228915+ograycode@users.noreply.github.com>
2023-09-15 14:53:08 -0400

    search wikipedia docs using a vector store for improved accuracy in rag.

--- src/routes.py
+++ src/routes.py
@@ -3,8 +3,11 @@ from langchain.chains import RetrievalQA
 from langchain.chains.llm import LLMChain
 from langchain.retrievers import WikipediaRetriever
 from langchain.schema.language_model import BaseLanguageModel
+from langchain.callbacks.manager import CallbackManagerForRetrieverRun
+from langchain.schema import Document
 from typing import Any, Dict, List
 from src.models import Route
+from src.vectore_store import in_memory
 from src.llm import get_llm
 
 
@@ -17,9 +20,18 @@ def default_route(llm: BaseLanguageModel):
 
 
 def wikipedia(llm: BaseLanguageModel):
-    docsearch = WikipediaRetriever()
+    docsearch = WikipediaVectorRetriever()
     return Route(
         name="wikipedia",
         description="wikipedia search",
         chain=RetrievalQA.from_llm(llm, retriever=docsearch, output_key="text")
     )
+
+
+class WikipediaVectorRetriever(WikipediaRetriever):
+    def _get_relevant_documents(
+        self, query: str, *, run_manager: CallbackManagerForRetrieverRun
+    ) -> List[Document]:
+        docs = super()._get_relevant_documents(query, run_manager=run_manager)
+        retriever = in_memory(docs).as_retriever()
+        return retriever.get_relevant_documents(query, callbacks=run_manager)
