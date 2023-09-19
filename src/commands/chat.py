cfe6e70d6909f4952961eab6d0fae91956c157c1
Jason O'Gray <1228915+ograycode@users.noreply.github.com>
2023-09-19 19:46:50 -0400

    Update the UI a bit to use a spinner.

--- src/commands/chat.py
+++ src/commands/chat.py
@@ -4,10 +4,11 @@ from src.ui import spinner, chat_loop
 
 def main():
 
+    # llm = get_llm()
+    # ai = create_ai(llm)
+    # ChatApp(ai).run()
     with spinner("building..."):
         llm = get_llm()
         ai = create_ai(llm)
         chain = ai.build_chain()
     chat_loop(chain, ai.routes)
-
-
--- src/ui.py
+++ src/ui.py
@@ -1,7 +1,9 @@
 from typing import Dict, List, Any
 from langchain.chains.base import Chain
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
-from rich.progress import Progress, SpinnerColumn, TextColumn, TimeElapsedColumn
+from langchain.schema import LLMResult
+from rich.table import Column
+from rich.progress import Progress, SpinnerColumn, TextColumn, TimeElapsedColumn, RenderableColumn
 from rich.prompt import Prompt
 from rich import print as r_print
 from contextlib import contextmanager
@@ -11,20 +13,19 @@ from rich.table import Table
 from src.fastchain import FastChain, Route
 
 @contextmanager
-def spinner(description: str, **kwargs):
+def spinner(description: str, *args, transient=False, **kwargs) -> Progress:
     with Progress(
         SpinnerColumn(),
         TextColumn("[progress.description]{task.description}"),
-        TimeElapsedColumn(),
-        transient=True,
+        *args,
         **kwargs
     ) as progress:
         progress.add_task(description, total=None)
         yield progress
 
-def prompt(txt: str = "[bold]>>>[/bold]"):
+def prompt(txt: str = "...."):
     return Prompt.ask(txt)
-    
+
 def print_ai(txt: str):
     r_print()
     r_print(f"[bold green]AI:[/bold green]", txt, end="")
@@ -45,16 +46,27 @@ def chat_loop(chain: Chain, routes: List[Route]):
 
     while True:
         query = prompt()
-        chain(query, callbacks=[StreamOut()])
-        r_print()
+        with spinner("[bold green]AI:[/bold green]",
+                     RenderableColumn(table_column=Column(overflow="fold")),
+                     transient=False) as spin:
+            chain(query, callbacks=[StreamOut(spin)])
         r_print()
 
     
 class StreamOut(StreamingStdOutCallbackHandler):
     
+    def __init__(self, spin: Progress, *args, **kwargs):
+        self._spin = spin
+        super().__init__(*args, **kwargs)
+    
     def on_llm_new_token(self, token: str, **kwargs: Any) -> None:
         """Run on new LLM token. Only available when streaming is enabled."""
-        r_print(token, end="")
+        self._spin.columns[-1].renderable += token
         
     def on_llm_start(self, serialized: Dict[str, Any], prompts: List[str], **kwargs: Any) -> None:
-        print_ai("")
+        pass
+        
+    def on_llm_end(self, response: LLMResult, **kwargs: Any) -> None:
+        self._spin.columns = (RenderableColumn(" "), ) + self._spin.columns[1:]
+        self._spin.refresh()
+        self._spin.stop()
