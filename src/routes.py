5e709689d37632b9a61209caf21f7a36fca3d218
Jason O'Gray <1228915+ograycode@users.noreply.github.com>
2023-09-26 19:22:15 -0400

    change import path to remove the warning.

--- src/routes.py
+++ src/routes.py
@@ -1,4 +1,4 @@
-from langchain import PromptTemplate
+from langchain.prompts import PromptTemplate
 from langchain.chains import RetrievalQA
 from langchain.chains.llm import LLMChain
 from langchain.retrievers import WikipediaRetriever
